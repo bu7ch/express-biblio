f4b08d3ee81e381d8028226b62b22a8a02cc77b6
Rodolphe <dutchr@live.fr>
2019-04-03 11:02:13 +0200

    finish function REST

--- src/route/book.js
+++ src/route/book.js
@@ -9,7 +9,6 @@ function getBooks(req, res){
     res.json(books);
   })
 }
-
 //POST book to save a new book
 function postBook(req, res) {
   let newBook = new Book(req.body);
@@ -18,5 +17,28 @@ function postBook(req, res) {
     res.json({msg:"ajout du book avec succès!", book})
   })
 }
+//GET /book/:id
+function getBook(req, res) {
+  Book.findById(req.params.id, (err, book) => {
+    if(err) res.send(err)
+    res.json(book)
+  })
+}
+//DELETE /book/:id
+function deleteBook(req, res) {
+  Book.remove({_id: req.params.id}, (err,result) => {
+    res.json({msg:"book bien supprimer", result})
+  })
+}
+// PUT /book/:id
+function updateBook(req,res) {
+  Book.findById({_id: req.params.id}, (err,book) => {
+    if(err) res.send(err)
+    Object.assign(book,req.body).save((err, book) =>{
+      if(err) res.send(err)
+      res.json({msg:"Book modifer",book})
+    });
+  });
+}
 
-module.exports = {getBooks, postBook};
+module.exports = {getBooks, postBook, getBook, deleteBook, updateBook};
